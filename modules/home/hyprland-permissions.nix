47841da9c6f5edde7806ac54d98e34054545d9dd
Ty <ogoxy.yt@gmail.com>
2026-09-06 08:53:39 -0400

    removed woomer from hyprland dynamic permissions homeModule apparently i already wrote this though may have to do one for niri but it is fixed for hyprland and it should now save and remember my Allow permissions for screenshotting and hyprshot/grim

--- modules/home/hyprland-permissions.nix
+++ modules/home/hyprland-permissions.nix
@@ -10,7 +10,6 @@
         "/nix/store/[a-z0-9]{32}-xdg-desktop-portal-hyprland-[0-9.]*/libexec/.*xdg-desktop-portal-hyprland.*, screencopy, allow"
         "/nix/store/[a-z0-9]{32}-hyprland-[0-9.]*/bin/hyprpm, plugin, allow"
         "/nix/store/[a-z0-9]{32}-hyprshot-[0-9.]*/bin/hyprshot, screencopy, allow"
-        "/nix/store/[a-z0-9]{32}-woomer-[0-9.]*/bin/woomer, screencopy, allow"
       ];
     };
   };
