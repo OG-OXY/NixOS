959d2d3e1d9f28ae3eb9749f16f58a18ed067a38
Ty <ogoxy.yt@gmail.com>
2026-09-15 02:40:16 -0400

    wired up window event loop and xdgshell for rust WM trying to make it so applications can open in window. currently in working state though with seat handler window event loop xdgshellstate compositor state window state etc.

--- Develop/Rust/src/main.rs
+++ Develop/Rust/src/main.rs
@@ -1,8 +1,9 @@
 mod window;
 
 use smithay::delegate_compositor;
-use smithay::delegate_shm;
 use smithay::delegate_seat;
+use smithay::delegate_shm;
+use smithay::delegate_xdg_shell;
 use smithay::input::{SeatHandler, SeatState};
 use smithay::reexports::wayland_server::backend::ClientData;
 use smithay::reexports::wayland_server::protocol::wl_buffer::WlBuffer;
@@ -10,6 +11,9 @@ use smithay::reexports::wayland_server::protocol::wl_surface::WlSurface;
 use smithay::reexports::wayland_server::Client;
 use smithay::wayland::buffer::BufferHandler;
 use smithay::wayland::compositor::{CompositorClientState, CompositorHandler, CompositorState};
+use smithay::wayland::shell::xdg::{
+    PositionerState, PopupSurface, ToplevelSurface, XdgShellHandler, XdgShellState,
+};
 use smithay::wayland::shm::{ShmHandler, ShmState};
 
 #[derive(Default)]
@@ -23,6 +27,8 @@ pub struct SmallWmState {
     pub compositor_state: CompositorState,
     pub shm_state: ShmState,
     pub seat_state: SeatState<SmallWmState>,
+    pub xdg_shell_state: XdgShellState,
+    pub toplevels: Vec<ToplevelSurface>,
 }
 
 impl CompositorHandler for SmallWmState {
@@ -60,12 +66,38 @@ impl SeatHandler for SmallWmState {
     }
 
     fn focus_changed(&mut self, _seat: &smithay::input::Seat<Self>, _focus: Option<&WlSurface>) {}
-    fn cursor_image(&mut self, _seat: &smithay::input::Seat<Self>, _image: smithay::input::pointer::CursorImageStatus) {}
+    fn cursor_image(
+        &mut self,
+        _seat: &smithay::input::Seat<Self>,
+        _image: smithay::input::pointer::CursorImageStatus,
+    ) {
+    }
+}
+
+impl XdgShellHandler for SmallWmState {
+    fn xdg_shell_state(&mut self) -> &mut XdgShellState {
+        &mut self.xdg_shell_state
+    }
+
+    fn new_toplevel(&mut self, surface: ToplevelSurface) {
+        surface.send_configure();
+        self.toplevels.push(surface);
+    }
+
+    fn new_popup(&mut self, _surface: PopupSurface, _positioner: PositionerState) {}
+    fn grab(
+        &mut self,
+        _surface: smithay::wayland::shell::xdg::PopupSurface,
+        _seat: smithay::reexports::wayland_server::protocol::wl_seat::WlSeat,
+        _serial: smithay::utils::Serial,
+    ) {}
+    fn reposition_request(&mut self, _surface: PopupSurface, _positioner: PositionerState, _token: u32) {}
 }
 
 delegate_seat!(SmallWmState);
 delegate_compositor!(SmallWmState);
 delegate_shm!(SmallWmState);
+delegate_xdg_shell!(SmallWmState);
 
 fn main() {
     window::run_window();
